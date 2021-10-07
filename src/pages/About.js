d0cf224b43b991fba55625544ad4d5570fe95860
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2021-10-06 21:08:16 -0300

    about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy
+        text ever since the 1500s, when an unknown printer took a galley
+        of type and scrambled it to make a type specimen book. 
+      </p>
+    </section>
   )
 }
 
