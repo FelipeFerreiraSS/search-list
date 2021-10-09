383728a982dfcbbf5340ae67800e781e0a58984b
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2021-10-08 23:24:14 -0300

    fetch drinks

--- src/context.js
+++ src/context.js
@@ -9,6 +9,43 @@ const AppProvider = ({ children }) => {
   const [searchTerm, setSearchTerm] = useState('a')
   const [cocktails, setCocktails] = useState([])
 
+  const fetchDrinks = async () => {
+    try{
+      const response = await fetch(`${url}${searchTerm}`)
+      const data = await response.json()
+      const {drinks} = data
+      if(drinks) {
+        const newCocktails = drinks.map((item) => {
+          const {
+            idDrink, 
+            strDrink, 
+            strDrinkThumb, 
+            strAlcoholic, 
+            strGlass
+          } = item
+          return {
+            id:idDrink, 
+            name:strDrink, 
+            image:strDrinkThumb, 
+            info:strAlcoholic, 
+            glass:strGlass
+          }
+        })
+        setCocktails(newCocktails)
+      }else {
+        setCocktails([])
+      }
+      setLoading(false)
+    } catch (error) {
+      console.error(error)
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchDrinks()
+  }, [searchTerm])
+
   return(
     <AppContext.Provider 
       value={{
