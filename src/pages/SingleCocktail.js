99d557df90e27db4f27558f12ebaaf2e92c8652e
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2021-10-10 22:59:31 -0300

    Update SingleCocktail.js

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -71,7 +71,35 @@ const SingleCocktail = () => {
   } = cocktail
   return (
     <section>
+      <Link to="/" className="btn btn-primary">
+        back
+      </Link>
       <h2 className="section-title">{name}</h2>
+      <div className="drink">
+        <img src={image} alt={name}/>
+        <div className="drink-info">
+          <p>
+            <span className="drink-data">name :</span>
+            {name}
+          </p>
+          <p>
+            <span className="drink-data">category :</span>
+            {category}
+          </p>
+          <p>
+            <span className="drink-data">info :</span>
+            {info}
+          </p>
+          <p>
+            <span className="drink-data">glass :</span>
+            {glass}
+          </p>
+          <p>
+            <span className="drink-data">instructions :</span>
+            {instructions}
+          </p>
+        </div>
+      </div>
     </section>
   )
 }
